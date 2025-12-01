89389607ff0bc6421d8fec721f9f5b7f465d5a37
Yahav <yahave225@gmail.com>
2025-12-01 20:06:05 +0200

    finished until safe and sound

---
+++ DebugWorkshop/SafeAndSound/part1.cpp
@@ -0,0 +1,25 @@
+#include "part1.h"
+#include <iostream>
+
+char* string_copy(char* dest, unsigned int destsize, char* src)
+{
+	char* ret = dest;
+	while (destsize > 0)
+	{
+		*dest++ = *src++;
+		destsize--;
+	}
+	return ret;
+}
+
+void part1()
+{
+	char password[] = "secret";
+	char dest[13];
+	char src[] = "hello world!";
+
+	string_copy(dest, 13, src);
+
+	std::cout << src << std::endl;
+	std::cout << dest << std::endl;
+}
